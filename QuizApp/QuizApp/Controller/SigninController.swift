4d6d902cf1649b15052e6e5aa9e53e58efa0b39a
お金 <57231517+baobaote00@users.noreply.github.com>
2021-06-16 08:08:51 -0700

    :heart: add containerview

---
+++ QuizApp/QuizApp/Controller/SigninController.swift
@@ -0,0 +1,30 @@
+//
+//  SigninController.swift
+//  QuizApp
+//
+//  Created by Nguyen Tien on 06/06/2021.
+//
+
+import UIKit
+
+class SigninController: UIViewController {
+
+    //MARK: - properties
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
